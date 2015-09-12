d3916077e3a07645e0b39f398c19c8e3cdf0e715
James M Snell <jasnell@gmail.com>
2015-09-12 13:59:15 -0700

    Fix app port env var

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const app = require('express')();
 
 app.get('/', (req, res)=>res.send('Hello World!'));
-const server = app.listen(process.env.PORT || 3000, ()=> {
+const server = app.listen(process.env.VCAP_APP_PORT || 3000, ()=> {
   let host = server.address().address;
   let port = server.address().port;
   if (host === '::') host = 'localhost';
